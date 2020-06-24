41adfb0658507c4c82a4e0704e946a268288519a
overdice <overdice@gmail.com>
2020-06-24 10:10:07 +0200

    version 1.0

--- script/script.js
+++ script/script.js
@@ -1,14 +1,3 @@
-// show to top button
-const btn = document.getElementById("to-top");
-window.onscroll = function() {showToTop()};
-function showToTop() {
-    if(document.body.scrollTop > 150 || document.documentElement.scrollTop > 150){
-        btn.style.display = "block";
-    } else {
-        btn.style.display = "none";
-    }
-}
-
 //pricelist
 const tabGelTehnik = document.getElementById("gel-tehnika");
 const tabNailArt = document.getElementById("nail-art");
