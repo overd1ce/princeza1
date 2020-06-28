1d0a346fdb7d9d1ac2058f35dce6f483420f751d
overdice <overdice@gmail.com>
2020-06-28 18:01:01 +0200

    responsive nav

--- script/totop.js
+++ script/totop.js
@@ -8,3 +8,24 @@ function showToTop() {
         btn.style.display = "none";
     }
 }
+
+const button = document.getElementById("nav-menu");
+const menu = document.getElementById("nav-menu-link");
+const menuClose = document.getElementById("close-menu");
+const navList = document.getElementById("nav-menu-link-every");
+
+const showMenu = () => {
+    menu.style.display = "block";
+    menuClose.style.display = "flex";
+    button.style.display = "none";
+    navList.style.flexDirection = "column";
+}
+const closeMenu = () => {
+    menu.style.display = "none";
+    menuClose.style.display = "none";
+    button.style.display = "block";
+    
+}
+
+button.addEventListener('click', showMenu);
+menuClose.addEventListener('click', closeMenu);
